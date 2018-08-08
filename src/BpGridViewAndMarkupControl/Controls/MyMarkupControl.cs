b92a12eb816cee47c6940a7bceadfe6f52edec0d
Vítězslav Stříbrný <vitezslav.stribrny@gmail.com>
2018-08-08 18:03:55 +0200

    issue repro created

---
+++ src/BpGridViewAndMarkupControl/Controls/MyMarkupControl.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.Controls;
+
+namespace BpGridViewAndMarkupControl.Controls
+{
+    public class MyMarkupControl : DotvvmMarkupControl
+    {
+    }
+}
+
---
+++ src/BpGridViewAndMarkupControl/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+using DotVVM.BusinessPack.Controls;
+
+namespace BpGridViewAndMarkupControl.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel, IMyMarkupControlViewMOdel
+    {
+        public string MyText { get; set; }
+
+
+        public BusinessPackDataSet<Item> DataSet { get; set; } = new BusinessPackDataSet<Item>();
+
+        public DefaultViewModel()
+        {
+            DataSet.Items = Enumerable.Range(0, 5).Select(x => new Item { Number = x }).ToList();
+        }
+
+        public void SetMyText()
+        {
+            MyText = DateTime.Now.ToString();
+        }
+    }
+
+    public class Item
+    {
+        public double Number { get; set; }
+    }
+
+    public interface IMyMarkupControlViewMOdel
+    {
+        void SetMyText();
+    }
+
+
+}
