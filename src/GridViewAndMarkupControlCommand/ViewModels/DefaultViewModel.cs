a2f12a4d1f68bfd2b4f9f8f9b207758ba0d4f096
Vítězslav Stříbrný <vitezslav.stribrny@gmail.com>
2018-09-25 11:17:06 +0200

    init commit

---
+++ src/GridViewAndMarkupControlCommand/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+using DotVVM.BusinessPack.Controls;
+
+namespace GridViewAndMarkupControlCommand.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+        public string MyText { get; set; }
+
+        public BusinessPackDataSet<Item> DataSet { get; set; } = new BusinessPackDataSet<Item>();
+
+        public DefaultViewModel()
+        {
+            DataSet.Items = Enumerable.Range(0, 5).Select(x => new Item { Number = x }).ToList();
+        }
+
+        public void OnValueChanged(int collectionIndex)
+        {
+            var item = DataSet.Items[collectionIndex];
+            MyText = item.Number.ToString();
+
+            if (collectionIndex == 0)
+            {
+                DataSet.Items[collectionIndex + 1].Number = item.Number + 1;
+            }
+        }
+    }
+
+
+    public class Item
+    {
+        public double Number { get; set; }
+    }
+}
