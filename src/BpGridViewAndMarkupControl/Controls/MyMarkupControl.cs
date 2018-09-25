83282a33b216831f532ec3043f9096532b38ccf9
Vítězslav Stříbrný <vitezslav.stribrny@gmail.com>
2018-09-25 12:24:10 +0200

    commandBinding bug in MarkupControl reproduced

--- src/BpGridViewAndMarkupControl/Controls/MyMarkupControl.cs
+++ src/BpGridViewAndMarkupControl/Controls/MyMarkupControl.cs
@@ -2,12 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DotVVM.Framework.Binding;
+using DotVVM.Framework.Binding.Expressions;
 using DotVVM.Framework.Controls;
 
 namespace BpGridViewAndMarkupControl.Controls
 {
     public class MyMarkupControl : DotvvmMarkupControl
     {
+        /// <summary>
+        /// Gets or sets the command that will be triggered when the control text is changed.
+        /// </summary>
+        public Command Changed
+        {
+            get { return (Command)GetValue(ChangedProperty); }
+            set { SetValue(ChangedProperty, value); }
+        }
+
+        public static readonly DotvvmProperty ChangedProperty =
+            DotvvmProperty.Register<Command, MyMarkupControl>(t => t.Changed, null);
+
     }
 }
 
